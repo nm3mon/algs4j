b0675466a489598f544f5a0ef2134e455970dece
Nabeel Ali Memon <nm3mon@gmail.com>
2013-06-20 17:11:21 -0400

    Article 3.1: Implementation and unit tests for simple sequential search implementation.

--- src/main/java/Article_3_1.java
+++ src/main/java/Article_3_1.java
@@ -38,4 +38,146 @@ public class Article_3_1 {
     //all keys in table, in sorted order
     Iterable<Key> keys();
   }
+  
+  public static abstract class AbstractST<Key extends Comparable<? super Key>, Value> 
+      implements ST<Key, Value> {
+    public AbstractST() {
+    }
+
+    @Override public abstract void put(Key key, Value val);
+
+    @Override public abstract Value get(Key key);
+
+    @Override public void delete(Key key) {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public boolean contains(Key key) {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public boolean isEmpty() {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public int size() {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public Key min() {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public Key max() {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public Key floor(Key key) {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public Key ceiling(Key key) {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public int rank(Key key) {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public Key select(int k) {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public void deleteMin() {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public void deleteMax() {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public int size(Key lo, Key hi) {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public Iterable<Key> keys(Key lo, Key hi) {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+
+    @Override public Iterable<Key> keys() {
+      throw new UnsupportedOperationException("Yet to be implemented");
+    }
+  } 
+  
+  public static class SequentialSearch<Key extends Comparable<? super Key>, Value> extends 
+      AbstractST<Key, Value> implements ST<Key, Value> {
+    
+    class Node {
+      Key key;
+      Value value;
+      Node next;
+      
+      Node(Key key, Value value, Node next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+      }
+
+    }
+    
+    int size = 0;
+    Node entryPointToInternalList;
+    
+    //search for the key and if found, update value. otherwise add key/value 
+    @Override public void put(Key key, Value val) {
+      Node internalList = entryPointToInternalList;
+      for (;internalList != null; internalList = internalList.next) {
+        //search hit
+        if (key.equals(internalList.key)) {
+          internalList.value = val;
+          size++;
+          return;
+        }
+      }
+      //search miss. add new node at the head
+      entryPointToInternalList = new Node(key, val, entryPointToInternalList);
+      size++;
+      return;
+    }
+
+    //search for the key and return associated value
+    @Override public Value get(Key key) {
+      Node internalList = entryPointToInternalList;
+      for (;internalList != null; internalList = internalList.next) {
+        if (key.equals(internalList.key)) {
+          return internalList.value;
+        }
+      }
+      return null;
+    }
+
+    @Override public int size() {
+      return size;
+    }
+
+    @Override public Iterable<Key> keys() {
+      return super.keys();
+    }
+
+    @Override public void delete(Key key) {
+      Node internalList = entryPointToInternalList;
+      //if the first node is to be deleted
+      if (internalList != null && internalList.key.equals(key)) {
+        entryPointToInternalList = internalList.next;
+        size--;
+        return;
+      }
+      //otherwise look for all next nodes to delete them before reaching them
+      if (internalList.next != null && internalList.next.key.equals(key)) {
+        internalList = internalList.next.next;
+        size--;
+        return;
+      }
+    }
+  }
 }
--- src/test/java/Article_3_1Test.java
+++ src/test/java/Article_3_1Test.java
@@ -1,5 +1,57 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author Nabeel Ali Memon
  */
-public class Article_3_1Test { 
+public class Article_3_1Test {
+  public static class SequentialSearchSTTest {
+    @Test public void shouldBeAbleToPutNewEntries() {
+      Article_3_1.ST<String, String> sequence = new Article_3_1.SequentialSearch<>();
+      Assert.assertEquals(0, sequence.size());
+      sequence.put("firstName", "Nabeel");
+      Assert.assertEquals(1, sequence.size());
+      sequence.put("middleName", "Ali");
+      Assert.assertEquals(2, sequence.size());
+      sequence.put("lastName", "Memon");
+      Assert.assertEquals(3, sequence.size());
+    }
+    
+    @Test public void shouldBeAbleToSearchAddedEntries() {
+      Article_3_1.ST<String, String> sequence = new Article_3_1.SequentialSearch<>();
+      Assert.assertNull(sequence.get("firstName"));
+      sequence.put("firstName", "Nabeel");
+      Assert.assertNotNull(sequence.get("firstName"));
+      Assert.assertEquals("Nabeel", sequence.get("firstName"));
+    }
+
+    @Test public void shouldBeAbleToUpdateEntries() {
+      Article_3_1.ST<String, String> sequence = new Article_3_1.SequentialSearch<>();
+      sequence.put("firstName", "Nabeel");
+      Assert.assertEquals("Nabeel", sequence.get("firstName"));
+      sequence.put("firstName", "Ummamah");
+      Assert.assertEquals("Ummamah", sequence.get("firstName"));
+    }
+    
+    @Test public void shouldBeAbleToRemoveFirstEntry() {
+      Article_3_1.ST<String, String> sequence = new Article_3_1.SequentialSearch<>();
+      sequence.put("firstName", "Nabeel");
+      Assert.assertEquals(1, sequence.size());
+      sequence.delete("firstName");
+      Assert.assertNull(sequence.get("firstName"));
+      Assert.assertEquals(0, sequence.size());
+    }
+
+    @Test public void shouldBeAbleToRemoveNonFirstEntry() {
+      Article_3_1.ST<String, String> sequence = new Article_3_1.SequentialSearch<>();
+      sequence.put("firstName", "Nabeel");
+      sequence.put("middleName", "Ali");
+      Assert.assertEquals(2, sequence.size());
+      sequence.delete("middleName");
+      Assert.assertNull(sequence.get("middleName"));
+      Assert.assertEquals(1, sequence.size());
+      sequence.delete("firstName");
+      Assert.assertEquals(0, sequence.size());
+    }
+  }
 }
